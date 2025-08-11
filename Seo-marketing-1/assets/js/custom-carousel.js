c80eaa667181362c38eaee54ee156223f64f01a1
Vishal Singh <singhvishal2977@gmail.com>
2025-08-11 14:39:19 +0530

    Add a seo marketing template

---
+++ Seo-marketing-1/assets/js/custom-carousel.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+    var owl = $('.leading_testimonial-section .owl-carousel');
+    owl.owlCarousel({
+        margin: 20,
+        nav: true,
+        loop: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 4500,
+        responsive: {
+            0: {
+                items: 1
+            },
+            360: {
+                items: 1
+            },
+            576: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 3
+            }
+        }
+    })
+})
+
+$(document).ready(function() {
+    var owl = $('.digital_service-section .owl-carousel');
+    owl.owlCarousel({
+        margin: 30,
+        nav: false,
+        loop: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 4500,
+        responsive: {
+            0: {
+                items: 1
+            },
+            360: {
+                items: 1
+            },
+            576: {
+                items: 2
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1200: {
+                items: 4
+            }
+        }
+    })
+})
